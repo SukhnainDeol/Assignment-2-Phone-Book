06cc988dbde4eb7e5ea93982212237707ef1a7c5
Sukhnain Deol <dsukhnain@gmail.com>
2023-01-24 14:18:39 -0800

    started

--- ListNode.java
+++ ListNode.java
@@ -1,1 +1,11 @@
+// Programmer: Sukhnain Deol
+// Class: CS 145
+// Date 01/24/2023
+// Assignment: Assignment 2: Phone Book
 
+// Purpose:
+
+class ListNode
+{
+
+} // end of ListNode class
--- TestClass.java
+++ TestClass.java
@@ -1,1 +1,14 @@
+// Programmer: Sukhnain Deol
+// Class: CS 145
+// Date 01/24/2023
+// Assignment: Assignment 2: Phone Book
 
+// Purpose:
+
+class TestClass
+{
+    public static void main(String[] args)
+    {
+
+    } // end of main method
+} // end of TestClass class
