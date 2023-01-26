7bf19aad093f5ea62418aaba0734d376e063f97d
Sukhnain Deol <101545430+TheCowCompany@users.noreply.github.com>
2023-01-26 04:29:42 +0000

    started

--- ListNode.java
+++ ListNode.java
@@ -7,5 +7,21 @@
 
 class ListNode
 {
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String city;
+    public inStringt phoneNumber;
+    public ListNode next;
+
+    public ListNode(String firstName, String lastName, String address, String city, String phoneNumber, ListNode next)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.next = next;
+    }
 
 } // end of ListNode class
--- PhonebookManager.java
+++ PhonebookManager.java
@@ -7,5 +7,35 @@
 
 class PhonebookManager
 {
+    private ListNode phoneBookEntry = new ListNode();
+    private ListNode lastNodeTracker = null;
 
+    public PhonebookManager()
+    {
+
+    }
+
+
+
+    public void newEntry()
+    {
+        if (phoneBookEntry.next == null)
+        {
+
+        }
+    }
+
+
+
+    public String toString()
+    {
+
+    }
+
+
+    private static void printChar(char printedChar, int printCount)
+    {
+        for(int i = 0; i < printCount; i++)
+            {System.out.print(printedChar);}
+    }
 } // end of PhonebookManager class
