66c0abc29a834953d43e42ef10762a176ab8de8d
Sukhnain Deol <101545430+TheCowCompany@users.noreply.github.com>
2023-02-01 20:52:17 +0000

    TestClass commented

--- TestClass.java
+++ TestClass.java
@@ -3,12 +3,11 @@
 // Date 01/24/2023
 // Assignment: Assignment 2: Phone Book
 
-// Purpose: Create an interface for users to access, manipulate, add and remove
-// data from a phonebook
+// Purpose: Create an interface for users to access, find, manipulate, 
+// add and remove data from a phonebook
 
-
-
-
+// test delete 
+// test default on editNode
 
 import java.util.*;
 
@@ -16,59 +15,54 @@ class TestClass
 {
     public static void main(String[] args)
     {
+        // creates test phonebookmanager object to showcase  
         PhonebookManager test = new PhonebookManager();
 
-        Scanner in = new Scanner(System.in);
-        boolean inUse = true;
+        Scanner in = new Scanner(System.in); // takes in user input
+        boolean inUse = true; // used to loop program until user quits
 
-        while(inUse)
+        while(inUse) // loops until 'quit' (or '0') is inputted by user
         {
-            printMenu();
-            String command = in.nextLine();
+            printMenu(); // prints command options to user
+            String command = in.nextLine(); // takes in users command
             
-            // if phonebook empty, ask them to created a new entry before doing anything else
+            // if phonebook empty, ask user to create a new entry before doing anything else
             if(test.size() == 0 && !(command.equals("1") || command.toLowerCase().equals("add entry")))
-            {   // if quitting, break out of nest if loop
+            {   // if quitting, break out of if statement (proceeds to switch/case)
                 if(command.equals("0") || command.toLowerCase().equals("quit"))
                     {break;}
                 System.out.println("\nPhonebook is empty, please create a a new entry first");
                 System.out.println("\nPress enter to contiue");
                 in.nextLine(); // waits for user to press enter 
-                System.out.println("\n");
-                continue; // repeats loop from beginning
+                System.out.println("\n"); // spacing to seperate previous input and menu
+                continue; // repeats whole loop from beginning
             }
             
+            // main part of the interface 
             switch(command.toLowerCase())
             {
                 case "1":
                 case "add entry":
-                    // turn into method
+                    // asks user for inputs and which are returned in an array
                     String[] newEntry = newEntryInput(in, test.size());
+                    // adds user inputs into a new entry in phonebook manager
                     test.add(newEntry[0], newEntry[1], newEntry[2], newEntry[3], newEntry[4], Integer.parseInt(newEntry[5])-1);
                     break;
                 case "2":
                 case "delete entry":
-                    if (test.size() == 0)
-                        {System.out.println("\nERROR: Phonebook is Empty");}
-                    else
-                    {
-                        System.out.print("\nEnter Contact Number to Delete: ");
-                        int deleteIndex;
-                        if(in.hasNextInt())
-                        { // deletes deletetIndex value from phonebook
-                            deleteIndex = in.nextInt();
-                            test.delete(deleteIndex-1);
-                        }
-                        else
-                            {System.out.println("ERROR: Input is not a Number");}
+                    System.out.print("\nEnter Contact Number to Delete: ");
+                    int deleteIndex; // index (starting from 1) of entry to delete
+                    if(in.hasNextInt()) // if int is inputted
+                    {   // deletes entry at deletetIndex value from phonebook
+                        deleteIndex = in.nextInt();
+                        test.delete(deleteIndex-1); // -1 because method starts with 0
                     }
+                    else // prints error message
+                        {System.out.println("ERROR: Input is not a Whole Number");}
                     break;
                 case "3":
                 case "view phonebook":
-                    if(test.size() == 0) // check if anything in phonebook
-                        {System.out.println("\nPhonebook is Empty");}
-                    else
-                        {System.out.println("\n" + test);} // print phonebook
+                    System.out.println("\n" + test); // prints phonebook 
                     break;
                 case "4":
                 case "search name":
@@ -144,38 +138,44 @@ class TestClass
                     break;
                 case "8":
                 case "edit name":
-                    editNode(in, "name", test);
+                    // asks which contact number's data to edit and what the new value is
+                    editNode(in, "name", test); 
                     break;
                 case "9":
                 case "edit address":
+                    // asks which contact number's data to edit and what the new value is
                     editNode(in, "address", test);
                     break;
                 case "10":
                 case "edit city":
+                    // asks which contact number's data to edit and what the new value is
                     editNode(in, "city", test);
                     break;
                 case "11":
                 case "edit phone number":
+                    // asks which contact number's data to edit and what the new value is
                     editNode(in, "phone number", test);
                     break;
                 case "0":
                 case "quit":
                     inUse = false; // ends while loop
                     continue; // continue instead of break in order to instantly end loop
-                default:
+                default: // default to sending general error message
                     System.out.println("\nERROR: Incorrect Input\n");
                     break;
             } // end of switch/case
 
             System.out.println("\nPress enter to continue");
-            in.nextLine(); // makes it easier to view output before next command
-            System.out.println("\n\n");
-        } // end of program while loop
+            in.nextLine(); // waits for user to press enter
+            // moves down to create space between menu and previous input
+            System.out.println("\n\n"); 
+        } // end of program while-loop
     } // end of main method
 
 
 
     // displays to the user what commands they have access to
+    // both number and command name will work in the program (upper or lower case)
     public static void printMenu()
     {
         System.out.println("Please choose one of the following menu options:");
@@ -196,15 +196,18 @@ class TestClass
     } // end of printMenu method
 
 
+    // parameters for scanner and size of phonebook in order for method to
+    // know valid range of contact numbers the user can input
 
     // asks user to input each field of the node (excluding next node)
     // as well as the contact number the new node will be shown as
+    // returns each of the values as a string array
     public static String[] newEntryInput(Scanner in, int phonebookSize) 
     {
-        boolean correctIndex = false;
-        String[] newEntry = new String[6]; // array to hold new entry 
+        // bool that loops until a valid contact # is inputted
+        boolean correctIndex = false; 
+        String[] newEntry = new String[6]; // array to hold new entry info
         
-        // get all info for new entry then use newNode method
         System.out.print("\nEnter First Name: ");
         newEntry[0] = in.next();
         in.nextLine(); // incase multi word input, clear line for next 
@@ -235,88 +238,91 @@ class TestClass
         while(correctIndex == false)
         { 
             System.out.print("Contact Number of New Entry (currently at " + phonebookSize + " contacts): ");
-            if(in.hasNextInt()) // if correct input, end loop 
+            if(in.hasNextInt()) // if int inputted
             {
-                newEntry[5] = in.next();
+                newEntry[5] = in.next(); // add new input
+                // parse int converts string to int 
                 int contactNumber = Integer.parseInt(newEntry[5]);
-                // if new entry is a contact number in phonebook
+                // if new entry is a contact number in phonebook or the next available
                 if(contactNumber <= phonebookSize+1 && contactNumber > 0)
-                    {correctIndex = true;}
+                    {correctIndex = true;} // ends loop
                 else
-                {
+                {   // tells user errors, size of phonebook and valid range of contact numbers
                     System.out.println("\nERROR: Index Not Within Than Phonebook Size of "+ phonebookSize);
                     System.out.println("Please choose a number through the range of 1 to " + (phonebookSize+1)+"\n");
                 }  
             }
-            else // print error message each failure
-                {System.out.println("ERROR: Please Give A Numerical Input");}
+            else // print error message 
+                {System.out.println("ERROR: Input is not a Whole Number");}
             in.nextLine(); // incase multi word input, clear line for next input
         }
-        
-        return newEntry;
+        return newEntry; // return String array
     } // end of newEntryInput method
 
+    // parameters are for scanner, which node data to ask the user about and the phonebook 
+    // manager in order to alter the data and know the size of the phonebook
 
-
-    public static void editNode(Scanner in, String nodeData, PhonebookManager test)
+    // asks user for what contact number's data they would like to change and then asks
+    // for the new value of that data
+    public static void editNode(Scanner in, String nodeData, PhonebookManager test) throws IllegalArgumentException
     {
-        String newData = "";
-        boolean correctIndexInputted = false;
+        String newData = ""; // new value of specific nodedata that will be changed
+        boolean correctIndexInputted = false; // check if valid index inputted
         while(!correctIndexInputted)
-        {
+        {   // asks for which contact number's data to edit
             System.out.print("Which contact number's "+nodeData+" would you like to edit? (currently at "+test.size()+" contacts): ");
-            if(in.hasNextInt())
+            if(in.hasNextInt()) // if an int inputted
             {
-                int editIndex = in.nextInt();
+                int editIndex = in.nextInt(); // takes in int
                 // if index is a real contact number
                 if(editIndex > 0 && editIndex <= test.size())
-                {
-                    correctIndexInputted = true;
-                    editIndex--; // index -1 to match index start 0 (shown as 1 to user)
-                    in.nextLine();
-                    // if not a changing name because it needs first and last name input
-                    if(!nodeData.equalsIgnoreCase("name"))
-                    {
-                        System.out.print("Enter new "+nodeData+": ");
-                        newData = in.nextLine();
-                    }
-                    
-                    switch(nodeData.toLowerCase())
-                    {
-                        case "name":
-                            System.out.print("Enter new first name: ");
-                            newData = in.nextLine();
-                            test.modifyFirstName(editIndex, newData); 
-
-                            System.out.print("Enter new last name: ");
-                            newData = in.nextLine();
-                            test.modifyLastName(editIndex, newData);
-
-                            break;
-                        case "address":
-                            test.modifyAddress(editIndex, newData);
-                            break;
-                        case "city":
-                            test.modifyCity(editIndex, newData);
-                            break;
-                        case "phone number":
-                            test.modifyPhoneNumber(editIndex, newData);
-                            break;
-                        default: // tells user they used an invalid parameter otherwise
-                            throw new IllegalArgumentException("Not a Valid NodeData Parameter");
-                    }
-                }
+                    {correctIndexInputted = true;} // ends loop
                 else
-                {
+                {   // sends error
                     System.out.println("ERROR: Number is not within range contacts");
-                    in.nextLine();
+                    in.nextLine(); // clears line for next input
                 }
             }
             else
-            {
+            {   // sends error
                 System.out.println("ERROR: Input is not a Whole Number");
-                in.nextLine();
+                in.nextLine(); // clears line for next input
             }
+        } // end of while loop
+
+        editIndex--; // index -1 to match index start 0 (shown as 1 to user)
+        in.nextLine(); // clears line for next input
+        // if not changing a name because it needs a first and last name input
+        if(!nodeData.equalsIgnoreCase("name"))
+        {   // asks for new values
+            System.out.print("Enter new "+nodeData+": ");
+            newData = in.nextLine(); // takes in new value
         }
+        
+        switch(nodeData.toLowerCase())
+        {   // name case asks for new first & last name then adds
+            case "name":
+                System.out.print("Enter new first name: ");
+                newData = in.nextLine();
+                test.modifyFirstName(editIndex, newData); 
+
+                System.out.print("Enter new last name: ");
+                newData = in.nextLine();
+                test.modifyLastName(editIndex, newData);
+
+                break;
+            case "address": // adds new address
+                test.modifyAddress(editIndex, newData);
+                break;
+            case "city": // adds new city
+                test.modifyCity(editIndex, newData);
+                break;
+            case "phone number": // adds new phone number
+                test.modifyPhoneNumber(editIndex, newData);
+                break;
+            default: // tells user they used an invalid parameter by default
+                throw new IllegalArgumentException("Not a Valid NodeData Parameter");
+                break;
+        } // end of switch/case
     } // end of editNode method
 } // end of TestClass class
